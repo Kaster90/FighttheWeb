d67ebb7751be9f2c6792c7a0376431ab363e927a
Kaster90 <pfeuffej@aston.ac.uk>
2015-11-07 23:23:30 +0100

    Update Fight.cs

--- Fight.cs
+++ Fight.cs
@@ -1,8 +1,25 @@
 class Fight{
-	public string Fight(){
-		enum Fightstages {wait4input, pattacks, eattacks, pgetshit, egetshit, pdead, edead }
+	public string StartFight(){
 		while(state == InCombat){
-			
+
+				//sometask as we wait for input; ////////////////has to be changed
+
+				Enemy.gethit(Hero.Attack(object[] Keywordstring, String Attack));
+
+				if(Enemies.alive()){
+					Hero.getHit(Enemey.Attack(object[] KeywordsValue));
+					if(Hero.alive()==0){
+						state = GameOver;
+						return state;
+						return 
+					}
+				}
+				else if(Enemies.alive()==0){
+					getloot();
+					state = OutofCombat;
+					return state;
+				}
+				
 		}	
 	}
 	
